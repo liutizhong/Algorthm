d0a8c09237c58416845ee0693a5b8160aab13e73
liutizhong <liutizhong@163.com>
2015-12-21 18:09:51 +0800

    绘图

--- src/python/yougou/chapter1/ChinaHadoop/GetHTMLUrl.py
+++ src/python/yougou/chapter1/ChinaHadoop/GetHTMLUrl.py
@@ -1,14 +1,26 @@
+# -*- coding: utf-8 -*-
 import re
 import urllib2
 __author__ = 'root'
 
-myUrl = "http://m.yougou.com"
+myUrl = "http://m.yougou.com/touch"
+req_header = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+
+req = urllib2.Request(myUrl, None, req_header)
+page = urllib2.urlopen(req)
+
 #response = urllib2.urlopen(myUrl)
-#the_page = response.read()
-the_page = '"<a href="http://gg/touch></a><a href="/touch/mem/m"> </a>""'
+the_page = page.read()
+the_page = the_page.decode('UTF-8')
+page.close()
+print the_page
+#the_page = '"<a href="http://gg/touch></a><a href="/touch/mem/m"> </a>""'
 mm = re.sub(r"(?<=href=\")/touch", "http://m.yougou.com", the_page)
+
+mms =re.sub(r"<a.+ ")
+
 #link_list = re.findall(r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')", the_page)
-link_list = re.findall(r"<a.+",mm)
+link_list = re.findall(r"<a.+", mm)
 for url in link_list:
     print url
 
